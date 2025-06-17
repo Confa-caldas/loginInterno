66a256a91e685d7c1731022806f9d71bf28a5fc1
Luis David Agudelo Rendon <tec_luisda@confamiliares.com>
2025-06-16 20:49:39 -0500

    Ajuste ruta validacion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   alojamiento: 'https://alojamiento.confa.co/alojamientoWS/rest/alojamiento/',
   identificacionFacial:
-    'https://app.confa.co:8687/transaccionAutenticacionWS/transaccion/',
+    'https://identidad.confa.co/transaccionAutenticacionWS/transaccion/',
   consultarIp: 'https://appint.confa.co/consultaIP/circular007/obtenerIP/',
   consultaLambdaDirecta: 'https://api-facial.confa.co/identificarvalidar',
   excepcionFacial:
